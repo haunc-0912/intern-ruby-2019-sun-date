84c3c226f2bc6ec932ac7f73c7d702db3b30bd70
haunguyen97 <nguyen.cong.hau@sun-asterisk.com>
2019-09-03 09:11:54 +0700

    Fix header, footer - layout login

--- app/assets/javascripts/config-uislider.js
+++ app/assets/javascripts/config-uislider.js
@@ -2,22 +2,23 @@ $(document).on('turbolinks:load', function () {
   var ageSlider = document.getElementById('age_slider');
   var startAge = $('#start_age');
   var endAge = $('#end_age');
+  if(ageSlider) {
+    noUiSlider.create(ageSlider, {
+      start: [20, 50],
+      tooltips: true,
+      format: wNumb({
+        decimals: 0
+      }),
+      range: {
+        'min': 18,
+        'max': 70
+      }
+    });
 
-  noUiSlider.create(ageSlider, {
-    start: [20, 50],
-    tooltips: true,
-    format: wNumb({
-      decimals: 0
-    }),
-    range: {
-      'min': 18,
-      'max': 70
-    }
-  });
-
-  ageSlider.noUiSlider.on('update', function (values, handle) {
-    let valueList = ageSlider.noUiSlider.get();
-    startAge.val(valueList[0]);
-    endAge.val(valueList[1]);
-  });
+    ageSlider.noUiSlider.on('update', function (values, handle) {
+      let valueList = ageSlider.noUiSlider.get();
+      startAge.val(valueList[0]);
+      endAge.val(valueList[1]);
+    });
+  }
 });
